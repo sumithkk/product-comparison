869d1008dfa21280726a9ee4ff0e562be54800e9
sumithkk <sumithkk2007@gmail.com>
2020-04-29 01:54:02 +0530

    UI changes

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -5,7 +5,7 @@ const ProductList = (props) =>
     {!props.display && <div className="closeProd" onClick={() => props.remove(props.sp)}>X</div>}
     <img alt="prod-img" src={props.data.images[props.sp]} />
     <h1>{props.data.titles[props.sp].title}</h1>
-    <h2>{props.data.titles[props.sp].subtitle}</h2>
+    {/* <h2>{props.data.titles[props.sp].subtitle}</h2> */}
     <div className="price" style={{ color: "#444" }}>
       <strong>₹{Math.trunc(props.data.productPricingSummary[props.sp].finalPrice)}</strong>
       <strong style={{ color: "#8f8f8f" }}>₹{Math.trunc(props.data.productPricingSummary[props.sp].price)}</strong>
